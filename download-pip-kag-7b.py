aa337df3d5a121b059b79fd43010308766d7f87d
pomegranar <an301@duke.edu>
2025-06-05 08:21:59 +0000

    Removed depricated resume_download option from model download scripts.

--- download-pip-kag-7b.py
+++ download-pip-kag-7b.py
@@ -2,8 +2,7 @@ from huggingface_hub import snapshot_download
 
 snapshot_download(
     repo_id="chengpingan/PIP-KAG-7B",
-    local_dir="./pip-kag-7b",
-    resume_download=True
+    local_dir="./Models/pip-kag-7b",
 )
 
 
