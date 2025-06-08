5c0ac20da95030487a3bf74a32617279cf820321
pomegranar <an301@duke.edu>
2025-06-08 18:36:46 +0800

    modified serve pipkag script.

--- pipkag-server/serve-pipkag.sh
+++ pipkag-server/serve-pipkag.sh
@@ -1,6 +1,6 @@
 sglang serve \
-  --model-path /home/pomegranar/PIP-KAG-CD/Models/pip-kag-7b \
+  --model-path /datapool/huggingface/hub/models--pip-kag-7b \
   --model-name pip-kag-7b \
   --dtype bf16 \
-  --port 8003
+  --port 8004
 
