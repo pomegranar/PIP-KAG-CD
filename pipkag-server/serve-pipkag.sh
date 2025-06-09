649d30d1b8f03b63be5fad643c2378a9f1defccb
pomegranar <an301@duke.edu>
2025-06-09 19:23:14 +0000

    New serve-pipkag.sh script in scripts directory.

--- pipkag-server/serve-pipkag.sh
+++
@@ -1,6 +0,0 @@
-sglang serve \
-  --model-path /datapool/huggingface/hub/models--pip-kag-7b \
-  --model-name pip-kag-7b \
-  --dtype bf16 \
-  --port 8004
-
---
+++ scripts/serve-pipkag.sh
@@ -0,0 +1,1 @@
+CUDA_VISIBLE_DEVICES=0 /home/pomegranar/PIP-KAG-CD/.venv/bin/python3.12 -m sglang.launch_server --model-path /datapool/huggingface/hub/models--pip-kag-7b --port 12420 --context-length 8192
